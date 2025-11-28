e9845b0ce1bd98d5eaefdb46001e680a27667909
salva <salvaarancegallego@gmail.com>
2025-11-28 15:18:14 +0100

    Añadir juego : Muertos y heridos

--- src/org/cuatrovientos/ed/Principal.java
+++ src/org/cuatrovientos/ed/Principal.java
@@ -3,6 +3,7 @@ package org.cuatrovientos.ed;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Cuatrovientos rules");	}
-
+		System.out.println("Cuatrovientos rules");
+		System.out.println("Somos los mejores");
+	}
 }
